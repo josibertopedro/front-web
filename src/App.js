9431e5393151a51283e73a31d32487b13c4b91f0
josibertopedro <112524878+josibertopedro@users.noreply.github.com>
2024-03-15 22:04:03 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Route1 from './route1';
+import './App.css';
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+   <h1> <Route1/></h1>  
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/route1.js
@@ -0,0 +1,18 @@
+import { FaUser, FaHome } from 'react-icons/fa';
+import { CiRedo } from "react-icons/ci";
+import img from './img/img1.png'
+function Route1() {
+    return (
+      <div className="route1">
+            <img src={img} alt="Descrição da imagem" />
+             
+            <h1><CiRedo /></h1>
+            
+          
+                
+      </div>
+    );
+  }
+  
+  export default Route1;
+  
